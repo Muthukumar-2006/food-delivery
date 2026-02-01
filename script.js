70aa5dbbc6db900782efd995f0b5917fef02f284
Muthukumar-2006 <kumarmuthu98603@gmail.com>
2026-02-01 08:19:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function sendMessage() {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("message").value;
+
+    if (name === "" || email === "" || message === "") {
+        alert("Please fill all fields");
+    } else {
+        alert("Thank you " + name + "! Your message has been sent.");
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+    }
+}
+
+
+
